ceaa26fa08775de0da2f6436b6e41361b584859f
deepanshujain92 <deepanshu.gv@gmail.com>
2017-06-20 01:41:33 +0530

    Added -Account -Dates -Balance -Config

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var config = require('./config');
 
 var app = express();
 
@@ -30,28 +31,42 @@ app.post('/webhook', function (req, res) {
     var action = (body.result.action);
     console.log(body);
 
-    if (action === 'getaccountdetails' && body.result.actionIncomplete === false)
+    if (action === 'balance' && body.result.actionIncomplete === false)
     {
         var request = require("request");
-
+       console.log(config);
         var options = {
             method: 'POST',
             url: 'http://52.172.213.166:8080/sbi/Account_List/api/EnqBancsAccountsList/',
             headers:
-                    {'postman-token': '2f62145f-9dea-cfb4-1b28-253d5f096ab7',
-                        'cache-control': 'no-cache',
-                        apikey: 'ywr4rQdbjSOVDtr',
-                        'content-type': 'application/json'},
-            body: {AccountNumber: body.result.parameters.AccountNumber},
-            json: true};
+                    {
+                         apikey: config.apiKey,
+                    },
+            body: {AccountNumber: config.AccountNumber},json:true        };
+        console.log(options);
 
-        request(options, function (error, response, body) {
-            console.log(body)
+        request(options, function (error, response) {
+            var apires = response.body;
+            var params = body.result.parameters;
+            var accountDetails = "";
+            for(var i=0;i<apires.ac_details.length;i++)
+            {
+                if(config.Accounts[params.AccountType]===apires.ac_details[i].AccountNumber)
+                {
+                    accountDetails = apires.ac_details[i];
+                    break;
+                }
+            }
+            console.log(body);
+        var speech;
+       // if(params.AccountType==="Savings") 
+            speech = "Here is balance for your "+params.AccountType+" Account " + accountDetails.AvailableBalance + accountDetails.Currency;
+//        else speech = "Here is Approved Sanctioned Amount for your "+params.AccountType+" Account" + accountDetails.ApprovedSanctionedAmount;
             if (error)
                 throw new Error(error);
             var json = {
-                "speech": "Welcome!" + body.main_ac.CIFname + ",\nYour PFid is " + body.main_ac.PFId,
-                "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+                "speech": speech,
+                "displayText": "",
                 "source": "DuckDuckGo"
             };
             res.set("Content-type", "application/json")
@@ -71,11 +86,10 @@ app.post('/webhook', function (req, res) {
             url: 'http://52.172.213.166:8080/sbi/Mini/api/EnqMiniStatement',
             headers:
                     {
-                        'cache-control': 'no-cache',
-                        apikey: 'ywr4rQdbjSOVDtr',
+                        apikey: config.apiKey,
                         'content-type': 'application/json'
                     },
-            body: {AccountNumber: "00000030001512992"},
+            body: {AccountNumber: '00000030001512992'},
             json: true
         };
         request(options, function (error, response, body) {
@@ -96,11 +110,14 @@ app.post('/webhook', function (req, res) {
             res.set("Content-type", "application/json");
             res.send(json);
         });
-        //console.log(body);
     } else if (action === 'accntstatement' && body.result.actionIncomplete === false)
     {
         var request = require("request");
-        var Period = body.result.parameters.Period;
+        var Period = body.result.parameters.Period;  
+        console.log(Period);
+        var FromDate = new Date(Period[0].split('/')[0]).toLocaleDateString();
+        var ToDate = new Date(Period[0].split('/')[1]).toLocaleDateString();
+
         console.log(JSON.stringify(Period));
         var options = {
             method: 'POST',
@@ -108,15 +125,15 @@ app.post('/webhook', function (req, res) {
             headers:
                     {
                         'cache-control': 'no-cache',
-                        apikey: 'ywr4rQdbjSOVDtr',
+                        apikey: config.apiKey,
                         'content-type': 'application/json'
                     },
            body: 
-   { AccountNumber: '30001512992',
-     FromAmount: 1000,
-     FromDate: '0',
-     ToAmount: '10000',
-     ToDate: '0',
+   { AccountNumber: config.AccountNumber,
+     FromAmount: "1000",
+     FromDate: FromDate,
+     ToAmount: "10000",
+     ToDate: ToDate,
      TransactionNumber: 150 },
   json: true };
         console.log(options);
@@ -124,11 +141,11 @@ app.post('/webhook', function (req, res) {
             if (error)
                 throw new Error(error);
             console.log(body);
-            var speech = "Sure, Your account  balance is . Here are your transactions during :";
+            var speech = "Sure. Here are your transactions between :"+FromDate+" - "+ToDate;
             for (var i = 0; i < body.StatementDetails.length; i++)
             {
                 speech = speech + "\n\
-" + (i + 1) + ". " + body.StatementDetails[i].Amount + " " + body.StatementDetails[i].ValueDate + " " + body.StatementDetails[i].Narration + "\n";
+" + (i + 1) + ". " + body.StatementDetails[i].Amount+body.StatementDetails[i].INBStatement + " " + body.StatementDetails[i].ValueDate + " " + body.StatementDetails[i].Narration + "\n";
             }
             var json = {
                 "speech": speech,
